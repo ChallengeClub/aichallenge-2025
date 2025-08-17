53b7eb94c13bf8acfe8f127bd3a91542968673f9
YU CHUNTAO <yuchuntao@hotmail.co.jp>
2025-08-17 15:19:43 +0900

    save

--- aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/src/simple_pure_pursuit.cpp
+++ aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/src/simple_pure_pursuit.cpp
@@ -89,6 +89,7 @@ void SimplePurePursuit::onTimer()
     // calc lateral control
     //// calc lookahead distance
     double lookahead_distance = lookahead_gain_ * target_longitudinal_vel + lookahead_min_distance_;
+    double lookahead_distance_2 = lookahead_distance + 2;
     //// calc center coordinate of rear wheel
     double rear_x = odometry_->pose.pose.position.x -
                     wheel_base_ / 2.0 * std::cos(odometry_->pose.pose.orientation.z);
@@ -101,32 +102,56 @@ void SimplePurePursuit::onTimer()
         return std::hypot(point.pose.position.x - rear_x, point.pose.position.y - rear_y) >=
                lookahead_distance;
       });
+    auto lookahead_point_itr_2 = std::find_if(
+      trajectory_->points.begin() + closet_traj_point_idx, trajectory_->points.end(),
+      [&](const TrajectoryPoint & point) {
+        return std::hypot(point.pose.position.x - rear_x, point.pose.position.y - rear_y) >=
+               lookahead_distance_2;
+      });
     if (lookahead_point_itr == trajectory_->points.end()) {
       lookahead_point_itr = trajectory_->points.end() - 1;
     }
+    if (lookahead_point_itr_2 == trajectory_->points.end()) {
+      lookahead_point_itr_2 = trajectory_->points.end() - 1;
+    }
+
     double lookahead_point_x = lookahead_point_itr->pose.position.x;
     double lookahead_point_y = lookahead_point_itr->pose.position.y;
-
-    geometry_msgs::msg::PointStamped lookahead_point_msg;
-    lookahead_point_msg.header.stamp = get_clock()->now();
-    lookahead_point_msg.header.frame_id = "map";
-    lookahead_point_msg.point.x = lookahead_point_x;
-    lookahead_point_msg.point.y = lookahead_point_y;
-    lookahead_point_msg.point.z = closet_traj_point.pose.position.z;
-    pub_lookahead_point_->publish(lookahead_point_msg);
+    double lookahead_point_x_2 = lookahead_point_itr_2->pose.position.x;
+    double lookahead_point_y_2 = lookahead_point_itr_2->pose.position.y;
 
     // calc steering angle for lateral control
     double alpha = std::atan2(lookahead_point_y - rear_y, lookahead_point_x - rear_x) -
                    tf2::getYaw(odometry_->pose.pose.orientation);
     double base = 2.0 * wheel_base_ * std::sin(alpha);
     double atan2result = std::atan2(base , lookahead_distance);
-    if(std::abs(atan2result) < 1){
-      atan2result = pow(atan2result, -3.0) * (std::abs(atan2result) / atan2result);
-    }else{
-      atan2result = pow(atan2result, 3.0) * (std::abs(atan2result) / atan2result);
+
+    double alpha_2 = std::atan2(lookahead_point_y_2 - rear_y, lookahead_point_x_2 - rear_x) -
+                   tf2::getYaw(odometry_->pose.pose.orientation);
+    double base_2 = 2.0 * wheel_base_ * std::sin(alpha_2);
+    double atan2result_2 = std::atan2(base_2 , lookahead_distance_2);
+
+    // determin which to use
+    RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000 /*ms*/, "atan2result %f", atan2result);
+    RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000 /*ms*/, "atan2result_2 %f", atan2result_2);
+    if( std::abs(atan2result_2) < 0.1
+        && atan2result / atan2result_2 > 0
+        // && std::abs(atan2result - atan2result_2) < 0.2
+        // && std::abs(atan2result) > std::abs(atan2result_2)
+    ){      
+      atan2result = atan2result_2;
+      lookahead_distance = lookahead_distance_2;
+      lookahead_point_x = lookahead_point_x_2;
+      lookahead_point_y = lookahead_point_y_2;
+      base = base_2;
+
     }
+
+
+    atan2result = pow(std::abs(atan2result), 1.3) * (std::abs(atan2result) / atan2result) * 2;
+
     cmd.lateral.steering_tire_angle =
-      steering_tire_angle_gain_ * std::atan2(base , lookahead_distance);
+      steering_tire_angle_gain_ * atan2result;
     if(current_longitudinal_vel < 4.1 && is_startup < 1) {
       cmd.lateral.steering_tire_angle = 0;
     }else if(current_longitudinal_vel < 4.45 && is_startup < 70){
@@ -142,7 +167,16 @@ void SimplePurePursuit::onTimer()
       is_startup = 2000;
       RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000 /*ms*/, "Finished startup");
     }
+
+    geometry_msgs::msg::PointStamped lookahead_point_msg;
+    lookahead_point_msg.header.stamp = get_clock()->now();
+    lookahead_point_msg.header.frame_id = "map";
+    lookahead_point_msg.point.x = lookahead_point_x;
+    lookahead_point_msg.point.y = lookahead_point_y;
+    lookahead_point_msg.point.z = closet_traj_point.pose.position.z;
+    pub_lookahead_point_->publish(lookahead_point_msg);
   }
+  
   pub_cmd_->publish(cmd);
   cmd.lateral.steering_tire_angle /=  steering_tire_angle_gain_;
   pub_raw_cmd_->publish(cmd);
