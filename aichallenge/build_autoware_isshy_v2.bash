d3da45a86480b07eef1c86539e1d74c0d4068e51
isshy-you(ubuntu) <isshy@kit.hi-ho.ne.jp>
2025-08-22 14:56:05 +0900

    * build_autoware_isshy_v2.bash で ファイル切り替えるようにした

---
+++ aichallenge/build_autoware_isshy_v2.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ ${1} == "clean" ]]; then
+    echo "clean build"
+    rm -r ./workspace/build/* ./workspace/install/*
+fi
+
+cp -p /aichallenge/workspace/src/aichallenge_system/autoware_overlay_rviz_plugin/src/speed_display_4digits.cpp /aichallenge/workspace/src/aichallenge_system/autoware_overlay_rviz_plugin/src/speed_display.cpp
+cp -p /aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/src/simple_pure_pursuit_isshy.cpp /aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/src/simple_pure_pursuit.cpp
+cp -p /aichallenge/workspace/src/aichallenge_submit/aichallenge_submit_launch/launch/reference.launch_isshy.xml /aichallenge/workspace/src/aichallenge_submit/aichallenge_submit_launch/launch/reference.launch.xml
+
+cd ./workspace || exit
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
